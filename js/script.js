7ff4c63d9f512290fae2942dd356c57cd04ed2a1
Hernan.Reiq <hernandemure1202@gmail.com>
2020-12-29 08:59:54 -0400

    empezamos a trabajar un poco con javascript

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 var lista_proyectos = document.getElementById('proyectos-portafolio').children;
 console.log(lista_proyectos);
+
+var div = document.createElement('div');
+div.setAttribute('class', 'ventada-emergente');
+console.log(div);
