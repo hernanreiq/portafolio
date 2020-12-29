74dbed00ecbbc071a05e757da8141f8194a3bb5e
Hernan.Reiq <hernandemure1202@gmail.com>
2020-12-29 14:19:10 -0400

    Ya funciona el overlay para el primer proyecto, se activa y desactiva al dar clic sobre el overlay

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 var lista_proyectos = document.getElementById('proyectos-portafolio').children;
-console.log(lista_proyectos);
 
 var div = document.createElement('div');
 div.setAttribute('class', 'ventada-emergente');
-console.log(div);
+
+var ventana_emergente_fondo = document.getElementById('ventana-emergente-fondo');
+document.getElementById('proyecto-1').addEventListener('click', EliminarScroll);
+function EliminarScroll(){
+   // document.getElementById('body').setAttribute('class', 'no-scroll');
+    ventana_emergente_fondo.setAttribute('style', 'visibility:visible');
+}
+
+ventana_emergente_fondo.addEventListener('click', ActivarScroll);
+function ActivarScroll(){
+    document.getElementById('body').setAttribute('class', 'scroll');
+    ventana_emergente_fondo.setAttribute('style', 'visibility:hidden');
+}
