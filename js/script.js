a6b30e446c02d9b0d202d8344d3347bd9b0e1557
Hernan.Reiq <hernandemure1202@gmail.com>
2020-12-29 19:37:04 -0400

    Primera ventana emergente en desarrollo, falta pulir su animacion y sus elementos internos, los botones de cambiar de imagen no están funcionando

--- js/script.js
+++ js/script.js
@@ -4,14 +4,32 @@ var div = document.createElement('div');
 div.setAttribute('class', 'ventada-emergente');
 
 var ventana_emergente_fondo = document.getElementById('ventana-emergente-fondo');
+var boton_cerrar_popup = document.getElementById('cerrar-popup');
+var popup = document.getElementById('popup');
+
+boton_cerrar_popup.addEventListener('click', ActivarScroll);
 document.getElementById('proyecto-1').addEventListener('click', EliminarScroll);
+
 function EliminarScroll(){
-   // document.getElementById('body').setAttribute('class', 'no-scroll');
-    ventana_emergente_fondo.setAttribute('style', 'visibility:visible');
+    document.getElementById('body').setAttribute('class', 'no-scroll');
+    VerPopUP();
+}
+function VerPopUP(){
+    ventana_emergente_fondo.classList.remove('no-visible');
+    ventana_emergente_fondo.classList.add('visible');
+    popup.classList.remove('no-visible');
+    popup.classList.add('visible');
 }
 
-ventana_emergente_fondo.addEventListener('click', ActivarScroll);
 function ActivarScroll(){
+    document.getElementById('body').classList.remove('no-scroll');
     document.getElementById('body').setAttribute('class', 'scroll');
-    ventana_emergente_fondo.setAttribute('style', 'visibility:hidden');
+    EsconderPopUp();
+}
+
+function EsconderPopUp(){
+    ventana_emergente_fondo.classList.remove('visible');
+    ventana_emergente_fondo.classList.add('no-visible');
+    popup.classList.remove('visible');
+    popup.classList.add('no-visible');
 }
