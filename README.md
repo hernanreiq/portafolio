8105cf83c2079089c995d6aed1ac3d65520552de
Reiq.Laptop <reiqvideos@gmail.com>
2021-01-30 03:11:46 -0400

    Ampliado el readme, agregado el proyecto de golden lottery, agregas nuevas animacioens con animate.css, se quitaron algunas animaciones de scrollreveal, se agregaron 4 habilidades nuevas

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# portafolio
-Este es mi portafolio de páginas, sitios y aplicaciones web que he creado.
+# Portafolio
+Este es mi portafolio de páginas, sitios y aplicaciones web que he creado. 
+
+# Enlace para verlo
+https://bit.ly/hernanreiq
+
+# Estado actual del portafolio
+Lo considero completado en cuanto a diseño, pero nunca descarto agregar animaciones o revisar su código para ver cuales secciones pueden mejorar.
 
 # Tecnologías usadas en la creación del portafolio
 HTML5, CSS3, JavaScript, Font-Awesome, Google Fonts, Bootstrap 4, ScrollReveal, Draw.io, Photoshop CS6, AJAX y JSON. 
--- js/animaciones.js
+++ js/animaciones.js
@@ -5,47 +5,15 @@ sr.reveal('#navBar', {
     distance: '100px',
     origin: 'top'
 });
-sr.reveal('#foto-presentacion', {
-    distance: '500px',
-    origin: 'right',
-    duration: 2000,
-    reset: true,
-    rotate: {
-        y: 50,
-        x: 30,
-        z: 20
-    }
-});
-sr.reveal('.presentacion-texto', {
-    distance: '500px',
-    origin: 'left',
-    duration: 2000,
-    reset: true,
-    rotate: {
-        y: -50,
-        x: -30,
-        z: 20
-    }
-});
 sr.reveal('#proyectos-portafolio', {
-    distance: '500px',
+    distance: '200px',
     origin: 'bottom',
     duration: 2000,
     scale: 0.2
-});
-sr.reveal('#lista-habilidades', {
-    distance: '500px',
-    origin: 'bottom',
-    duration: 2000
 });
 sr.reveal('.contenedor-texto-informacion', {
-    distance: '500px',
+    distance: '200px',
     origin: 'bottom',
     duration: 2000,
     scale: 0.2
-});
-sr.reveal('#lista-contacto', {
-    distance: '500px',
-    origin: 'bottom',
-    duration: 2000
 });
