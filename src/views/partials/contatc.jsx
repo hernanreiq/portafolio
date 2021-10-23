3556677959e12ec1c412adc7533f7aab5185f695
Reiq.Laptop <reiqvideos@gmail.com>
2021-10-23 17:07:51 -0400

    Portafolio - React : Agregado mi correo en la sección de contacto

--- src/views/partials/contatc.jsx
+++ src/views/partials/contatc.jsx
@@ -1,45 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faInstagram, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-function Contact() {
-    return (
-        <React.Fragment>
-            <a name="contacto" className="ancla"></a>
-            <section id="section-contacto">
-                <div className="titulo titulo-gradiente-azul mi-contacto">
-                    <h2>Contacto</h2>
-                </div>
-                <div className="blanco"></div>
-                <div className="contenido contacto">
-                    <ul id="lista-contacto" className="animate__animated animate__lightSpeedInRight">
-                        <li className="shadow">
-                            <a href="https://www.linkedin.com/in/hernan-demorizi-ure%C3%B1a-4430031b1/" target="_blank">
-                                <FontAwesomeIcon icon={faLinkedin} className="icono" />
-                                <br /> LinkedIn</a>
-                        </li>
-                        <li className="shadow">
-                            <a href="#contacto" id="boton-email">
-                                <FontAwesomeIcon icon={faEnvelope} className="icono" />
-                                <br /> Email</a>
-                        </li>
-                        <li className="shadow">
-                            <a href="https://www.instagram.com/hernan.reiq" target="_blank">
-                                <FontAwesomeIcon icon={faInstagram} className="icono" />
-                                <br /> Instagram</a>
-                        </li>
-                        <li className="shadow">
-                            <a href="https://github.com/hernanreiq" target="_blank">
-                                <FontAwesomeIcon icon={faGithub} className="icono" />
-                                <br /> GitHub</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="clearfix"></div>
-            </section>
-        </React.Fragment>
-    )
+class Contact extends Component {
+    sendEmail = () => {
+        var data = {
+            id: 'hernandemure1202',
+            domain: 'gmail.com',
+            subject: '?subject=¡Acabo de ver tu portafolio!'
+        }
+        window.open(`mailto:${data.id}@${data.domain + data.subject}`);
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <a name="contacto" className="ancla"></a>
+                <section id="section-contacto">
+                    <div className="titulo titulo-gradiente-azul mi-contacto">
+                        <h2>Contacto</h2>
+                    </div>
+                    <div className="blanco"></div>
+                    <div className="contenido contacto">
+                        <ul id="lista-contacto" className="animate__animated animate__lightSpeedInRight">
+                            <li className="shadow">
+                                <a href="https://www.linkedin.com/in/hernan-demorizi-ure%C3%B1a-4430031b1/" target="_blank" rel="noreferrer">
+                                    <FontAwesomeIcon icon={faLinkedin} className="icono" />
+                                    <br />LinkedIn</a>
+                            </li>
+                            <li className="shadow">
+                                <button onClick={this.sendEmail}>
+                                    <FontAwesomeIcon icon={faEnvelope} className="icono" />
+                                    <br />Email
+                                </button>
+                            </li>
+                            <li className="shadow">
+                                <a href="https://www.instagram.com/hernan.reiq" target="_blank" rel="noreferrer">
+                                    <FontAwesomeIcon icon={faInstagram} className="icono" />
+                                    <br />Instagram</a>
+                            </li>
+                            <li className="shadow">
+                                <a href="https://github.com/hernanreiq" target="_blank" rel="noreferrer">
+                                    <FontAwesomeIcon icon={faGithub} className="icono" />
+                                    <br />GitHub</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="clearfix"></div>
+                </section>
+            </React.Fragment>
+        )
+    }
 }
 
 export default Contact;
